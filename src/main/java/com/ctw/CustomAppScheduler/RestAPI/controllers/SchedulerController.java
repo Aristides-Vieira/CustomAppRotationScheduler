866137dda54e940f89d3c7ca6b04a352cccf9ec8
Aris <aristidesv3@gmail.com>
2021-10-17 17:23:14 +0100

    vacations and inactive redone

--- src/main/java/com/ctw/CustomAppScheduler/RestAPI/controllers/SchedulerController.java
+++ src/main/java/com/ctw/CustomAppScheduler/RestAPI/controllers/SchedulerController.java
@@ -4,9 +4,7 @@ import com.ctw.CustomAppScheduler.RestAPI.models.Scheduler;
 import com.ctw.CustomAppScheduler.RestAPI.repositories.SchedulerRepository;
 import com.ctw.CustomAppScheduler.RestAPI.services.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
